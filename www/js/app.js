35c5999690f8faf27d0c2ca7f5fe785596265675
Thiago Henrique <Thiago Henrique>
2017-04-16 02:29:17 -0300

    Finalização Aplicativo fBrandão

--- www/js/app.js
+++ www/js/app.js
@@ -198,6 +198,5 @@ angular.module('main', ['ionic', 'controllers'])
       }
     })
     ;
-    // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/main');
 });
